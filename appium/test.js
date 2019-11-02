18d18cfc83ed4a06e9617250c1d9eff668b2b68f
Taweechai Maklay <m.taweechai@gmail.com>
2019-11-02 20:57:13 +0700

    Updated package.json and test.js.

--- appium/test.js
+++ appium/test.js
@@ -2,15 +2,25 @@ const wdio = require('webdriverio');
 const assert = require('assert');
 const find = require('appium-flutter-finder');
 
+const osSpecificOps = process.env.APPIUM_OS === 'android' ? {
+  platformName: 'Android',
+  deviceName: 'emulator-5554',
+  // @todo support non-unix style path
+  app: __dirname +  '/../build/app/outputs/apk/debug/app-debug.apk',
+}: process.env.APPIUM_OS === 'ios' ? {
+  platformName: 'iOS',
+  platformVersion: '12.2',
+  deviceName: 'iPhone X',
+  noReset: true,
+  app: __dirname +  '/../ios/Runner.zip',
+
+} : {};
+
 const opts = {
   port: 4723,
   capabilities: {
-    //...osSpecificOps,
-      platformName: 'Android',
-      deviceName: 'emulator-5554',
-      automationName: 'Flutter',
-      app: __dirname + '/../build/app/outputs/apk/debug/app-debug.apk'
-
+    ...osSpecificOps,
+    automationName: 'Flutter'
   }
 };
 
